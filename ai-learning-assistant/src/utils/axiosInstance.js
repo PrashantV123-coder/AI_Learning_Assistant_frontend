a9e56a228ccb81e5d1ea304265a68604ce12b2e8
Prashant Vishwakarma <prashantvishwakarma1516@gmail.com>
2026-05-28 16:38:54 +0530

    setting base url of backend

--- ai-learning-assistant/src/utils/axiosInstance.js
+++ ai-learning-assistant/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://ai-learning-assistant-backend-3slg.vercel.app/",
 });
 
 axiosInstance.interceptors.request.use(
